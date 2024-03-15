f7d37d6ae872741ef15721d56873076ceea7bb77
Zibeline <zibeline.dev@gmail.com>
2024-03-15 16:27:20 +0100

    ✨ Save & Load system

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -23,3 +23,9 @@ var data : Data
 func _enter_tree() -> void:
 	_singleton_check()
 	data = Data.new()
+	SaveSystem.load_data()
+
+
+## Triggered when the save timer completes a loop. Save the game.
+func _on_save_timer_timeout() -> void:
+	SaveSystem.save_data()
