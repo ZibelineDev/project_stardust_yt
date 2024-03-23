f56f886ecbecec7bfec53372054d43742cc922b5
Zibeline <zibeline.dev@gmail.com>
2024-03-23 18:02:57 +0100

    🧹 Cleaning deprecated code from user interface scene

--- scenes/user_interface/user_interface.gd
+++ scenes/user_interface/user_interface.gd
@@ -5,9 +5,6 @@ extends Control
 
 ## List of views.
 enum Views {
-	PROTOTYPE_GENERATOR,
-	PROTOTYPE_CLICKER,
-	PROTOTYPE_UPGRADES,
 	UNIVERSE,
 	CONSCIOUSNESS_CORE,
 }
@@ -19,22 +16,6 @@ signal navigation_requested(view : Views)
 ## Navigate to Universe View on Launch.
 func _ready() -> void:
 	navigation_requested.emit(Views.UNIVERSE)
-
-
-
-## Triggered when the prototype generator link is clicked.
-func _on_prototype_generator_link_pressed() -> void:
-	navigation_requested.emit(Views.PROTOTYPE_GENERATOR)
-
-
-## Triggered when the prototype clicker link is clicked.
-func _on_prototype_clicker_link_pressed() -> void:
-	navigation_requested.emit(Views.PROTOTYPE_CLICKER)
-
-
-## Triggered when the prototype upgrades link is clicked.
-func _on_prototype_upgrades_link_pressed() -> void:
-	navigation_requested.emit(Views.PROTOTYPE_UPGRADES)
 
 
 ## Triggered when the Consciousness Core link is clicked.
